0ccd8a8ad460a3351fecc56402f04d0d0252e079
Asen Angelov <aangelov@clustermarket.com>
2022-05-31 09:30:53 +0300

    add basic style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App';
+import './reset.css';
+import './App.scss';
+
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="main">
+        <Sidebar />
+        <div className="container"></div>
+      </div>
     </div>
   );
 }
