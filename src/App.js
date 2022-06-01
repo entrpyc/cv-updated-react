baae2cf3a56fe45d94967f2e921fc84184675f3a
Asen Angelov <aangelov@clustermarket.com>
2022-06-01 09:10:50 +0300

    include expereince in the page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './reset.css';
 import './App.scss';
 
 import Sidebar from './components/Sidebar/Sidebar';
+import Experience from './components/Experience/Experience';
 
 function App() {
   return (
     <div className="App">
       <div className="main">
         <Sidebar />
-        <div className="container"></div>
+        <div className="container">
+          <Experience />
+        </div>
       </div>
     </div>
   );
