3afa2e2723a76ab5960a60959f699503e6a621a5
Asen Angelov <aangelov@clustermarket.com>
2022-06-01 10:40:55 +0300

    add portfolio components

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,31 @@
+import './Portfolio.scss';
+
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/GlobalContext';
+
+import PortfolioBlock  from './components/PortfolioBlock/PortfolioBlock';
+
+function Portfolio() {
+  const { portfolio } = useContext(GlobalContext)
+  const { webDevelopment, animationAndGraphicDesign } = portfolio
+
+  return (
+    <section id="portfolio">
+      <h2>Portfolio</h2>
+      <div className="wrap">
+        <h3>Web Development</h3>
+        {webDevelopment.map((item, index) => (
+          <PortfolioBlock {...item} key={index} />
+        ))}
+      </div>
+      <div className="wrap">
+        <h3>Web Development</h3>
+        {animationAndGraphicDesign.map((item, index) => (
+          <PortfolioBlock {...item} key={index} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Portfolio
---
+++ src/components/Portfolio/components/PortfolioBlock/PortfolioBlock.js
@@ -0,0 +1,23 @@
+import './PortfolioBlock.scss';
+
+function PortfolioBlock({ title, href, external }) {
+  return (
+    <div className="portfolio-block">
+      <a href={href} className={`block video ${!external ? 'single' : ''}`}>
+        <div className="icon">
+          <img src="./assets/video-icon.png" alt={title + ' video preview'} />
+        </div>
+        <p>{title}</p>
+      </a>
+      {external ?
+        <a href={external} className="block external">
+          <div className="icon">
+            <img src="./assets/external-icon.png" alt={title + ' external link'} />
+          </div>
+        </a>
+      :null}
+    </div>
+  )
+}
+
+export default PortfolioBlock
