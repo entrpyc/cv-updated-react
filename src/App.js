77d1fdbb22dcab58d93b7befcef0b520108c7aea
Asen Angelov <aangelov@clustermarket.com>
2022-06-01 11:05:23 +0300

    include about section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.scss';
 import { GlobalProvider } from './context/GlobalContext'
 
 import Sidebar from './components/Sidebar/Sidebar';
+import About from './components/About/About';
 import Experience from './components/Experience/Experience';
 import Portfolio from './components/Portfolio/Portfolio';
 
@@ -14,6 +15,7 @@ function App() {
         <div className="main">
           <Sidebar />
           <div className="container">
+            <About />
             <Experience />
             <Portfolio />
           </div>
